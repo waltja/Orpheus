0b0acbd1b0ea403d43f4ecfc6bd101a9c70e25c5
T3 FRC Team 7312 <79057169+t3team-laptop@users.noreply.github.com>
2024-02-29 13:50:23 -0600

    d

--- src/main/java/frc/robot/autos/AutoShooterStop.java
+++ src/main/java/frc/robot/autos/AutoShooterStop.java
@@ -19,12 +19,13 @@ public class AutoShooterStop extends Command {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    shooter.stop();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    shooter.stop();
   }
 
   // Called once the command ends or is interrupted.
