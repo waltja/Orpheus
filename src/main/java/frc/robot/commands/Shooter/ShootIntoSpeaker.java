144be9e5ee0cf97c0d35c042f1be726ca65cdf47
Akshita <akshitasantra@gmail.com>
2024-03-22 07:08:00 -0500

    New shooter code!

--- src/main/java/frc/robot/commands/Shooter/ShootIntoSpeaker.java
+++ src/main/java/frc/robot/commands/Shooter/ShootIntoSpeaker.java
@@ -14,12 +14,14 @@ public class ShootIntoSpeaker extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    shooter.setShooterSpeed(1.0);
+    shooter.setShooterLSpeed(-.75);
+    shooter.setShooterRSpeed(-.75);
   }
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    shooter.stop();
+    shooter.stopL();
+    shooter.stopR();
   }
   // Returns true when the command should end.
   @Override
